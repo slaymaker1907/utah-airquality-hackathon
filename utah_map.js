42e15df646c5386e71e0e913b3a0e0ff35729401
slaymaker1907 <dyllongagnier@gmail.com>
2015-10-23 22:09:10 -0600

    Gave capability to display two data sets on maps

--- utah_map.js
+++ utah_map.js
@@ -13,14 +13,10 @@ var AirDataPoint = function(date, magnitude, measType, location)
 
 var dataPoint = new AirDataPoint(new Date(2015, 10, 23), 100, "test", {lat:39, lng:-112});
 var allDataPoints = [dataPoint, new AirDataPoint(new Date(2015, 10, 23), 50, "test", {lat:39, lng:-111}), new AirDataPoint(new Date(2015, 10, 23), 100, "test", {lat:39, lng:-110})];
+var allDataPoints2 = [dataPoint];
 
 function update()
 {
-}
-
-var AirQualityMap = function()
-{
-	this.allDataPoints = [];
 }
 
 function initMap()
@@ -41,12 +37,12 @@ function initMap()
   var button = document.getElementById("update");
   var MAX_SIZE = 60000;
   
-  function getNormalizeFactor()
+  function getNormalizeFactor(dataPts)
   {
 	  var maxSize = 0;
-	  for(var i = 0; i < allDataPoints.length; i++)
+	  for(var i = 0; i < dataPts.length; i++)
 	  {
-		  var dataPt = allDataPoints[i];
+		  var dataPt = dataPts[i];
 		  if (dataPt.magnitude > maxSize)
 			  maxSize = dataPt.magnitude;
 	  }
@@ -69,18 +65,38 @@ function initMap()
 	});
 	circles[i] = cityCircle;
   }
+  circles2 = []; // An array of 100 circles to be used at will.
+  for(var i = 0; i < maxNumCircles; i++)
+  {
+	  var cityCircle = new google.maps.Circle({
+	  strokeColor: '#FF0000',
+	  strokeOpacity: 0.8,
+	  strokeWeight: 2,
+	  fillColor: '#FF0000',
+	  fillOpacity: 0.35,
+	  map: map2,
+	  center: {lat: 39, lng: -112},
+	  radius: 60000
+	});
+	circles2[i] = cityCircle;
+  }
   
   function updateMap()
   {
-	  factor = getNormalizeFactor();
+	  factor = getNormalizeFactor(allDataPoints);
+	  factor2 = getNormalizeFactor(allDataPoints2);
 	  clearCircles();
-	  var i = 0;
-	  for(var i2 = 0; i2 < allDataPoints.length; i2++)
+	  for(var i = 0; i < allDataPoints.length; i++)
 	  {
-		  options = getSpecifiedOptions(allDataPoints[i2].magnitude * factor, allDataPoints[i2].location);
+		  options = getSpecifiedOptions(allDataPoints[i].magnitude * factor, allDataPoints[i].location);
 		  circles[i].setOptions(options)
 		  circles[i].setMap(map1);
-		  i++;
+	  }
+	  for(var i = 0; i < allDataPoints2.length; i++)
+	  {
+		  options = getSpecifiedOptions(allDataPoints2[i].magnitude * factor, allDataPoints2[i].location);
+		  circles2[i].setOptions(options)
+		  circles2[i].setMap(map2);
 	  }
   }
   
@@ -89,6 +105,10 @@ function initMap()
 	  for(var i = 0; i < circles.length; i++)
 	  {
 		  circles[i].setMap(null);
+	  }
+	  for(var i = 0; i < circles2.length; i++)
+	  {
+		  circles2[i].setMap(null);
 	  }
   }
   
