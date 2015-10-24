f437b4bdd3dc5296c9c4296528dd13ffc4aad300
slaymaker1907 <dyllongagnier@gmail.com>
2015-10-23 19:53:20 -0600

    Updated code to be able to display air data points

--- utah_map.js
+++ utah_map.js
@@ -1,3 +1,14 @@
+var AirDataPoint = function(date, magnitude, measType, location)
+{
+	this.date = date;
+	this.magnitude = magnitude;
+	this.measType = measType;
+	this.location = location;
+}
+
+var dataPoint = new AirDataPoint(new Date(2015, 10, 23), 100, "test", {lat:39, lng:-112});
+var allDataPoints = [dataPoint];
+
 function update()
 {
 }
@@ -12,22 +23,70 @@ function initMap()
   });
   
   var button = document.getElementById("update");
+  var MAX_SIZE = 60000;
+  
+  function getNormalizeFactor()
+  {
+	  var maxSize = 0;
+	  for(dataPt in allDataPoints)
+		  if (dataPt.magnitude > maxSize)
+			  maxSize = dataPt.magnitude;
+	  return MAX_SIZE * maxSize;
+  }
+  
   function updateMap()
   {
-	  // Do stuff to update the map.
+	  factor = getNormalizeFactor();
+	  clearCircles();
+	  var i = 0;
+	  for(pt in allDataPoints)
+	  {
+		  options = getSpecifiedOptions(pt.magnitude * factor, pt.location);
+		  circles[i].setOptions(options)
+		  circles[i].setMap(map);
+	  }
   }
-  google.maps.event.addDomListener(button, 'click', updateMap);
-    
-  cityCircle = new google.maps.Circle({
+  
+  function clearCircles()
+  {
+	  for(circle in circles)
+	  {
+		  circle.setMap(null);
+	  }
+  }
+  
+  function getSpecifiedOptions(radius, location)
+  {
+	  return {
+		  strokeColor: '#FF0000',
+		  strokeOpacity: 0.8,
+		  strokeWeight: 2,
+		  fillColor: '#FF0000',
+		  fillOpacity: 0.35,
+		  map: null,
+		  center: location,
+		  radius: radius
+	  };
+  }
+  
+  var circles = []; // An array of 100 circles to be used at will.
+  for(i = 0; i < 100; i++)
+  {
+	  cityCircle = new google.maps.Circle({
 	  strokeColor: '#FF0000',
 	  strokeOpacity: 0.8,
 	  strokeWeight: 2,
 	  fillColor: '#FF0000',
 	  fillOpacity: 0.35,
-	  map: map,
+	  map: null,
 	  center: {lat: 39, lng: -112},
-	  radius: 20000
+	  radius: 60000
 	});
+	circles.push(cityCircle);
+  }
+  
+  google.maps.event.addDomListener(button, 'click', updateMap);
+  updateMap();
 }
 
 function notifyMapChanged()
