b526ae648fa4243da6ee5aff140d954666aa9c63
slaymaker1907 <dyllongagnier@gmail.com>
2015-10-23 19:16:57 -0600

    Updated map to be dynamic

--- utah_map.js
+++ utah_map.js
@@ -11,11 +11,12 @@ function initMap()
     zoom: 6
   });
   
-  var centerControlDiv = document.createElement('div');
-  var centerControl = new CenterControl(centerControlDiv, map);
-
-  centerControlDiv.index = 1;
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
+  var button = document.getElementById("update");
+  function updateMap()
+  {
+	  // Do stuff to update the map.
+  }
+  google.maps.event.addDomListener(button, 'click', updateMap);
     
   cityCircle = new google.maps.Circle({
 	  strokeColor: '#FF0000',
@@ -29,32 +30,7 @@ function initMap()
 	});
 }
 
-function CenterControl(controlDiv, map) {
-  // Set CSS for the control border.
-  var controlUI = document.createElement('div');
-  controlUI.style.backgroundColor = '#fff';
-  controlUI.style.border = '2px solid #fff';
-  controlUI.style.borderRadius = '3px';
-  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-  controlUI.style.cursor = 'pointer';
-  controlUI.style.marginBottom = '22px';
-  controlUI.style.textAlign = 'center';
-  controlUI.title = 'Click to recenter the map';
-  controlDiv.appendChild(controlUI);
-
-  // Set CSS for the control interior.
-  var controlText = document.createElement('div');
-  controlText.style.color = 'rgb(25,25,25)';
-  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-  controlText.style.fontSize = '16px';
-  controlText.style.lineHeight = '38px';
-  controlText.style.paddingLeft = '5px';
-  controlText.style.paddingRight = '5px';
-  controlText.innerHTML = 'Update';
-  controlUI.appendChild(controlText);
-
-  // Setup the click event listeners: simply set the map to Chicago.
-  controlUI.addEventListener('click', function() {
-    cityCircle.setMap(null)
-  });
+function notifyMapChanged()
+{
+	document.getElementById("update").click();
 }
